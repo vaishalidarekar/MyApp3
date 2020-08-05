948cea0c40622a92da899c77a45526a31f568eee
VaishaliDarekar <vaishalirdarekar@gmail.com>
2020-08-05 10:57:21 +0530

    commit TestProduct Java

---
+++ MyApp3/src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,13 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p1=new Product("OFSS", 100);
+		System.out.println(p1.productName);
+		System.out.println(p1.productPrice);
+		
+	}
+
+}
