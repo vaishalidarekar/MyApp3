2ae99c919eeaa86db2f016069a06ff669b77c975
VaishaliDarekar <vaishalirdarekar@gmail.com>
2020-08-05 11:17:06 +0530

    changes in TestProduct.java

--- MyApp3/src/main/java/com/ofss/TestProduct.java
+++ MyApp3/src/main/java/com/ofss/TestProduct.java
@@ -3,11 +3,9 @@ package com.ofss;
 public class TestProduct {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Product p1=new Product("OFSS", 100);
-		System.out.println(p1.productName);
-		System.out.println(p1.productPrice);
-		
+		Product p=new Product("SmartPhone",45000);
+		System.out.println(p.getProductName()+" and "+p.getProductPrice());
+
 	}
 
 }
