2691c37983560dba03c4c1189d6249cd07049e4a
VaishaliDarekar <vaishalirdarekar@gmail.com>
2020-08-06 13:47:41 +0530

    Committed standalone Java changes in MyApp3 using Jenkin

--- MyApp3/src/main/java/com/ofss/TestProduct.java
+++ MyApp3/src/main/java/com/ofss/TestProduct.java
@@ -5,6 +5,9 @@ public class TestProduct {
 	public static void main(String[] args) {
 		Product p=new Product("SmartPhone",45000);
 		System.out.println(p.getProductName()+" and "+p.getProductPrice());
+		System.out.println("jenkin build");
+		System.out.println("this is jenkin build and working");
+		System.out.println("Integration Github to Jenkin Aug 6-2020");
 
 	}
 
