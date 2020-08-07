b10b2dc7a0e5d532ee70df03bce9bea69caa8caf
VaishaliDarekar <vaishalirdarekar@gmail.com>
2020-08-07 13:48:36 +0530

    Try 3 after removing compilation errors

--- MyApp3/src/main/java/com/ofss/TestProduct.java
+++ MyApp3/src/main/java/com/ofss/TestProduct.java
@@ -3,12 +3,14 @@ package com.ofss;
 public class TestProduct {
 
 	public static void main(String[] args) {
-		Product p=new Product("SmartPhone",45000);
-		System.out.println(p.getProductName()+" and "+p.getProductPrice());
+		
+		//Product p=new Product("SmartPhone",45000.00);
+		
+		//System.out.println(p.getProductName()+" and "+p.getProductPrice());
 		System.out.println("jenkin build");
 		System.out.println("this is jenkin build and working");
 		System.out.println("Integration Github to Jenkin Aug 6-2020");
-		System.out.printlin ("2nd try");
+		System.out.println ("2nd try");
 
 	}
 
